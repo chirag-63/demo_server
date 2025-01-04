c9afdcc5cd0ee82850da8c932ebf110308fc1fd9
chirag <chiragchauhanx@gmail.com>
2025-01-05 00:54:16 +0530

    better inputs

--- models/zodSchema.js
+++ models/zodSchema.js
@@ -1,15 +1,15 @@
 const z = require("zod");
 
 const signupSchema = z.object({
-  name: z.string(),
-  username: z.string(),
+  name: z.string().min(1),
+  username: z.string().min(2),
   password: z
     .string()
     .min(6, { message: "password must be 6 or more characters long" }),
 });
 
 const loginSchema = z.object({
-  username: z.string(),
+  username: z.string().min(2),
   password: z.string().min(6, {
     message: "password must be 6 or more characters long",
   }),
