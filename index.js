e197e012a1897c1b56bce29843b3c95264aab46d
chirag <chiragchauhanx@gmail.com>
2025-01-04 07:20:00 +0530

    fixed cors error

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 const router = require('./routes');
+const cors = require('cors');
 app.use(express.json());
 
+app.use(cors())
 app.use("/api/v1", router);
 
 app.listen(port, () => {
